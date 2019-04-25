ecc1d58b6a0f3d78568d4f382ec260bd744f0ea8
ronsun13 <43909205+ronsun13@users.noreply.github.com>
2019-04-24 20:43:03 -0400

    Update initialize.m

--- ib_matlab_3D/initialize.m
+++ ib_matlab_3D/initialize.m
@@ -1,21 +1,21 @@
 %initialize.m
 
-L=1.0
-N=32
-h=L/N
+L=1.0 %domain length
+N=32  %mesh number 
+h=L/N %mesh width
 ip=[(2:N),1]
 im=[N,(1:(N-1))]
 
 
-K=0.1
-rho=1
-mu=0.01
-tmax=4
-dt=0.01
+K=0.1 %spring constant between boundary points
+rho=1 %density
+mu=0.01 %viscosity
+tmax=4 %simulation time
+dt=0.01 %timestep
 clockmax=ceil(tmax/dt)
 
 
-[X v]=trisph(1/3,1/2,4);
+[X v]=trisph(1/3,1/2,4); %trisph(r,a,nref) creates a triangulated sphere of raidus r, centered at a and nref times refined.
 tri=triangulation(v,X);
 
 numtri=size(tri,1);
@@ -38,7 +38,7 @@ for ei=1:numed
     elength=elength+el;
 end
 u=zeros(N,N,N,3);
-% initial perturbation
+% initial perturbation, here is a plane wave.
 au1=[0 0 1];
 ku1=[0 1 0];
 au2=[1 0 0];
